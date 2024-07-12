d2c4ad5241bc642f8e1de219a621ecb893692cf9
Nayanjyoti Sharma <113464044+NayanXS@users.noreply.github.com>
2024-07-12 19:07:48 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber =Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//notes
+
+//"33" => 33
+//"33abs" => NaN
+//true =>1 ; false => 0
+
+
+let isLoggedIn = "NayanXS"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//"nayanXS"=>true; ""=>false
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
