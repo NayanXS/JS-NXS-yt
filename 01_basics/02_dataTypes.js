009dcd79fc40ac92b96d01c04fe34ce291d944be
Nayanjyoti Sharma <113464044+NayanXS@users.noreply.github.com>
2024-07-12 18:48:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("3 + 3") //  we ar using nodejs , not browser
+
+
+console.log(3+3)
+
+console.log("NayanXS")
+
+
+let name = "NayanXS"
+let age = 18
+let isLoggedIn= false
+
+//number=> 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined) //undefined
